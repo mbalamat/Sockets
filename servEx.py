2ca5133123e608873c673a9f4ea27e32a7f05300
mbalamat <mbalamatsias@gmail.com>
2015-03-08 02:43:53 +0200

    Changed to the specifications BillTziv gave me

--- servEx.py
+++ servEx.py
@@ -3,15 +3,15 @@
 import SocketServer
 
 class MyTCPHandler(SocketServer.BaseRequestHandler):
-    
+
     def handle(self):
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
         print "{} wrote:".format(self.client_address[0])
         print self.data
-        
+
         #self.request.sendall(self.data.upper())
-        self.request.sendall("I am a simple tcp server made by Marios and I am answering your request....!")
+        self.request.sendall("OK")
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 9999
